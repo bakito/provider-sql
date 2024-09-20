6d36f7f9f14ab4ce5961b42de6bd910ddc90eff1
bakito <github@bakito.ch>
2024-09-20 11:33:41 +0200

    update build file

--- make-package.sh
+++ make-package.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+make generate
+
 cat package/crossplane.yaml > _output/package.yaml
 
 for crd in $(ls package/crds); do
   echo "---" >> _output/package.yaml
   cat "./package/crds/${crd}" >> _output/package.yaml
 done
+
+make build
