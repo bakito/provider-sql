2239cfeba82525d594a82e06881db09d54e357f3
bakito <github@bakito.ch>
2024-09-20 08:41:33 +0200

    addd image build work around

---
+++ make-package.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cat package/crossplane.yaml > _output/package.yaml
+
+for crd in $(ls package/crds); do
+  echo "---" >> _output/package.yaml
+  cat "./package/crds/${crd}" >> _output/package.yaml
+done
